d09b7571f4eed9e9afc69b76eeef1a4f84c933be
StephenFinegan21 <x20191952@student.ncirl.com>
2022-03-14 19:58:57 +0000

    installing dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+
+import { FaBeer } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className=" font-bold underline">
+      <h3> Lets go for a <FaBeer />? </h3>
+    </h1>
     </div>
   );
 }
